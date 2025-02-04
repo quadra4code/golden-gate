d8bf7e2be856543501f8c9e51c59f8613e55c121
Mahmoud Elsonbaty <mahmoudelsonbaty777@gmail.com>
2025-02-04 18:56:24 +0200

    configure vercel
    add wsgi line app = application
    add vercel.json configuration file

--- goldengate_royalestate/wsgi.py
+++ goldengate_royalestate/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'goldengate_royalestate.settings')
 
 application = get_wsgi_application()
+
+app = application
