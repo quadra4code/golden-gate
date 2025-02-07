4a47991e93e7141f21c591ca3f0f10a6df06b6f0
Kareem <mahmoudelsonbaty777@gmail.com>
2025-02-08 01:49:44 +0200

    Update settings.py
    remove *

--- goldengate_royalestate/settings.py
+++ goldengate_royalestate/settings.py
@@ -35,7 +35,6 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'golden-gate-three.vercel.app']
 CORS_ALLOWED_ORIGINS = [
     "https://golden-beit.vercel.app",
     "http://localhost:3000"
-    "*"
 ]
 # CORS_ALLOW_ALL_ORIGINS = True
 # CORS_ALLOW_CREDENTIALS = True
